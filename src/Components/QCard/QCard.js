40b8f4a988d5790ca1e9ab7d0698074fd15675fe
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-01-14 20:14:22 -0500

    Added Comments and a few css Mods

--- src/Components/QCard/QCard.js
+++ src/Components/QCard/QCard.js
@@ -1,75 +1,20 @@
 import React, { useState } from 'react'
 import './QCard.css'
+import questions from '../../data'
 
 
 
 export default function QCard() {
 
-    let questions = [
-        {
-            question: 'What type of skin/coat do you have?',
-            options:['Fur', 'Scales', 'Feathers']
-        },
-        {
-            question: 'What color is your fur?',
-            options:['Brown', 'White', 'Black']
-        },
-        {
-            question: 'Do you bark?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'What Are You?' ,
-            options:[]          
-        },
-        {
-            question: 'Do you live on land or water?',
-            options:['Water', 'Land']
-        },
-        {
-            question: 'Are you a fish?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'do you fly?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'What size are you?',
-            options:['Small', 'Large']
-        },
-        {
-            question: 'Are you a Penguin?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'Are you a Black Bear?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'Thats Awesome!!',
-            options:[]
-        },
-        {
-            question: 'Are you a Rabbit?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'Are you an Eagle?',
-            options:['Yes', 'No']
-        },
-        {
-            question: 'Are you a Lizard?',
-            options:['Yes', 'No']
-        },
-
-        
-    ]
+    // State
 
     const [currentQuestion, setCurrentQuestion] = useState(0);
 
     const  [previousQuestion] = useState([0])
 
+    // Functions
+
+    // Goes back 1 question
     function back() {
         if(previousQuestion.length > 1){
             
@@ -78,99 +23,102 @@ export default function QCard() {
         }
     }
 
+    // Resets Form to first Question
     function reset(){
         setCurrentQuestion(0)
     }
 
+    // Function updates currentQuestion When button 1 is clicked
     let updateQ = () => {
         previousQuestion.push(currentQuestion)
         
         if(currentQuestion === 0){
             setCurrentQuestion(1)
         }
-        if(currentQuestion === 1  ){
+        else if(currentQuestion === 1  ){
             setCurrentQuestion(2)
         }
-        if(currentQuestion === 2 ){
+        else if(currentQuestion === 2 ){
             setCurrentQuestion(3)
         }       
-        if(currentQuestion === 4 ){
+        else if(currentQuestion === 4 ){
             setCurrentQuestion(5)
         }
-        if(currentQuestion === 9 ){
+        else if(currentQuestion === 9 ){
             setCurrentQuestion(10)
         }
-        if(currentQuestion === 11 ){
+        else if(currentQuestion === 11 ){
             setCurrentQuestion(10)
         }
-        if(currentQuestion === 5 ){
+        else if(currentQuestion === 5 ){
             setCurrentQuestion(10)
         }
-        if(currentQuestion === 8 ){
+        else if(currentQuestion === 8 ){
             setCurrentQuestion(10)
         }
-        if(currentQuestion === 6 ){
+        else if(currentQuestion === 6 ){
             setCurrentQuestion(12)
         }
-        if(currentQuestion === 8 ){
+        else if(currentQuestion === 8 ){
             setCurrentQuestion(10)
         }
-        if(currentQuestion === 12 ){
+        else if(currentQuestion === 12 ){
             setCurrentQuestion(10)
         }
-        if(currentQuestion === 13 ){
+        else if(currentQuestion === 13 ){
             setCurrentQuestion(10)
         }
 
     }
 
+// Function updates currentQuestion When button 1 is clicked
     let updateQ2 = () => {
         previousQuestion.push(currentQuestion)
         
         if(currentQuestion === 0){
             setCurrentQuestion(4)
         }
-     
-        if(currentQuestion === 5 ){
+        else if(currentQuestion === 5 ){
             setCurrentQuestion(3)
         }
-        if(currentQuestion === 2 ){
+        else if(currentQuestion === 2 ){
             setCurrentQuestion(3)
         }
-        if(currentQuestion === 9 ){
+        else if(currentQuestion === 9 ){
             setCurrentQuestion(3)
         }
-        if(currentQuestion === 11 ){
+        else if(currentQuestion === 11 ){
             setCurrentQuestion(3)
         }
-        if(currentQuestion === 1 ){
+        else if(currentQuestion === 1 ){
             setCurrentQuestion(11)
         }
-        if(currentQuestion === 6 ){
+        else if(currentQuestion === 6 ){
             setCurrentQuestion(8)
         }
-        if(currentQuestion === 8 ){
+        else if(currentQuestion === 8 ){
             setCurrentQuestion(3)
         }
-        if(currentQuestion === 12 ){
+        else if(currentQuestion === 12 ){
             setCurrentQuestion(3)
         }
-        if(currentQuestion === 4 ){
+        else if(currentQuestion === 4 ){
             setCurrentQuestion(13)
         }
-        if(currentQuestion === 13 ){
+        else if(currentQuestion === 13 ){
             setCurrentQuestion(3)
         }
 
     }
 
+    // Function updates currentQuestion When button 3 is clicked
     let updateQ3 = () => {
         previousQuestion.push(currentQuestion)
         
         if(currentQuestion === 1){
             setCurrentQuestion(9)
         }
-        if(currentQuestion === 0){
+        else if(currentQuestion === 0){
             setCurrentQuestion(6)
         }
    
@@ -178,36 +126,40 @@ export default function QCard() {
 
     return (
         <>
-        <p>GUESS WHO</p>
+        <h1>GUESS WHO</h1>
         <div className='card'>
             
-            <h1 className='card__question'>{questions[currentQuestion].question}</h1>
-            {/* {questions[currentQuestion].options.map((option,index) =>{
-                return  <button className='card__btn' key={index}>
-                            <h2>{option}</h2>
-                        </button>
-            })} */}
-            
+            <h2 className='card__question'>
+                {questions[currentQuestion].question}
+            </h2>
             
             {questions[currentQuestion].options[0] ?
-                 <button className='card__btn' onClick={updateQ}>
-                <h2>{questions[currentQuestion].options[0]}</h2>
-            </button>: ''}
+                <button className='card__btn' 
+                        onClick={updateQ}>
+                    <h2>{questions[currentQuestion].options[0]}</h2>
+                </button>: ''}
             {questions[currentQuestion].options[1] ?
-                 <button className='card__btn' onClick={updateQ2} >
-                <h2>{questions[currentQuestion].options[1]}</h2>
-            </button>: ''}
+                <button className='card__btn' 
+                        onClick={updateQ2} >
+                    <h2>{questions[currentQuestion].options[1]}</h2>
+                </button>: ''}
             {questions[currentQuestion].options[2] ?
-                 <button className='card__btn' onClick={updateQ3}>
-                <h2>{questions[currentQuestion].options[2]}</h2>
-            </button>: ''}
-            {/* {currentQuestion === 4 ? <h1>IDK</h1>: ''} */}
+                <button className='card__btn' 
+                        onClick={updateQ3}>
+                    <h2>{questions[currentQuestion].options[2]}</h2>
+                </button>: ''}
             <div>
 
-                {currentQuestion === 0 ? '' : <button className='card__btn__back' onClick={back}>
-                    <h2>back</h2></button> }
-                {currentQuestion === 0 ? '' : <button className='card__btn__back' onClick={reset}>
-                    <h2>reset</h2></button> }
+                {currentQuestion === 0 ? '' : 
+                    <button className='card__btn__option' 
+                            onClick={back}>
+                        <h2>back</h2>
+                    </button> }
+                {currentQuestion === 0 ? '' : 
+                    <button className='card__btn__option' 
+                            onClick={reset}>
+                        <h2>reset</h2>
+                    </button> }
                 
             </div>
            
--- src/data.js
+++ src/data.js
@@ -1,26 +1,61 @@
-let questionData = [
-        {
-            question: 'What type of coat/skin do you have?',
-            options:['FUR',
-                    'FEATHERS',
-                    'SCALES'
-                    ]
-        },
-        {
-            question: 'What type of coat/skin do you have?',
-            options:['FUR',
-                    'FEATHERS',
-                    'SCALES'
-                    ]
-        },
-        {
-            question: 'What type of coat/skin do you have?',
-            options:['FUR',
-                    'FEATHERS',
-                    'SCALES'
-                    ]
-        },
-    ]
+let questions = [
+    {
+        question: 'What type of skin/coat do you have?',
+        options:['Fur', 'Scales', 'Feathers']
+    },
+    {
+        question: 'What color is your fur?',
+        options:['Brown', 'White', 'Black']
+    },
+    {
+        question: 'Do you bark?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'What Are You?' ,
+        options:[]          
+    },
+    {
+        question: 'Do you live on land or water?',
+        options:['Water', 'Land']
+    },
+    {
+        question: 'Are you a fish?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'do you fly?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'What size are you?',
+        options:['Small', 'Large']
+    },
+    {
+        question: 'Are you a Penguin?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'Are you a Black Bear?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'Thats Awesome!!',
+        options:[]
+    },
+    {
+        question: 'Are you a Rabbit?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'Are you an Eagle?',
+        options:['Yes', 'No']
+    },
+    {
+        question: 'Are you a Lizard?',
+        options:['Yes', 'No']
+    },
 
-
-export default questionData;
+    
+]
+export default questions
